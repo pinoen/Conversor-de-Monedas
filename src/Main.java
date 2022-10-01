d404ac1b9b2db712b13b64b61824495446123db8
Emilio Pino <pinoen@yahoo.com.ar>
2022-10-01 17:14:10 -0300

    dentro de bloque while se agrega try and catch para que aparezca mensaje de error en caso de que no se ingresen valores validos (numericos)

--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,26 @@ public class Main {
         boolean isActive = true;
 
         while(isActive){
-            String[] conversor = { "Moneda", "Temperatura" };
-            String eleccion = JOptionPane.showInputDialog(null,
-                    "Seleccione una opcion de conversion", "Menu",
-                    JOptionPane.INFORMATION_MESSAGE, null,
-                    conversor, conversor[0]).toString();
+            try {
+                String[] conversor = { "Moneda", "Temperatura" };
+                String eleccion = JOptionPane.showInputDialog(null,
+                        "Seleccione una opcion de conversion", "Menu",
+                        JOptionPane.INFORMATION_MESSAGE, null,
+                        conversor, conversor[0]).toString();
 
-            switch (eleccion){
-                case "Moneda":
-                    String monto = JOptionPane.showInputDialog("Por favor ingrese el monto de dinero a convertir");
-                    resultado.convertir(Double.parseDouble(monto));
-                    break;
-                case "Temperatura":
-                    String temp = JOptionPane.showInputDialog("Por favor ingrese el valor de temperatura a convertir");
-                    temperatura.convertir(Double.parseDouble(temp));
+                switch (eleccion){
+                    case "Moneda":
+                        String monto = JOptionPane.showInputDialog("Por favor ingrese el monto de dinero a convertir");
+                        resultado.convertir(Double.parseDouble(monto));
+                        break;
+                    case "Temperatura":
+                        String temp = JOptionPane.showInputDialog("Por favor ingrese el valor de temperatura a convertir");
+                        temperatura.convertir(Double.parseDouble(temp));
+                }
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null, "Valor ingresado no valido", "ERROR", JOptionPane.ERROR_MESSAGE);
             }
+
 
             int continuar = JOptionPane.showConfirmDialog(null,
                     "Desea continuar", "Elija una opcion", JOptionPane.YES_NO_OPTION);
